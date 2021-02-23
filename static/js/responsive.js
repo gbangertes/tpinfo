c75e8cd656d3f232d6dda3cfc63b9266d068ed37
gab <gbangertes@gmail.com>
2021-02-23 20:30:06 -0300

    Agregada barra de navegacion y app noticias

---
+++ static/js/responsive.js
@@ -0,0 +1,8 @@
+function respScreen() {
+  var x = document.getElementById("MyTopnav");
+  if (x.className === "topnav") {
+	x.className += " responsive";	
+  } else {
+	x.className = "topnav";
+  }
+}
--- tpinfo/settings/base.py
+++ tpinfo/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.noticias',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
