9a74b335756a41076ce2604582da9cf1c22bcc8c
gab <gbangertes@gmail.com>
2021-03-05 10:08:34 -0300

    Corregido error en el repositorio

---
+++ apps/usuarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import  path
+
+from apps.usuarios import views
+
+urlpatterns = [
+    path('registro/', views.registro, name="registro"),
+    path('login/', views.login, name="login"),
+]
+
